a05f41d66c754fda9fb6316a2ac092bbea728874
Mg <gekoncze@seznam.cz>
2023-04-02 11:30:14 +0200

    fixed wrong commit

--- test/cz/mg/tokenizer/AllTests.java
+++ test/cz/mg/tokenizer/AllTests.java
@@ -1,10 +1,8 @@
 package cz.mg.tokenizer;
 
 import cz.mg.annotations.classes.Test;
-import cz.mg.tokenizer.utilities.CharacterReaderTest;
 
 public @Test class AllTests {
     public static void main(String[] args) {
-        CharacterReaderTest.main(args);
     }
 }
